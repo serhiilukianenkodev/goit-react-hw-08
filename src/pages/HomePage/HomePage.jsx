a7f525661a30f5c5f670bd4d75f4056416fbec23
serhiilukianenkodev <99186322+serhiilukianenkodev@users.noreply.github.com>
2025-05-11 12:20:31 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,12 +11,7 @@ export default function HomePage() {
   return (
     <>
       <div style={styles.container}>
-        <h1 style={styles.title}>
-          Task manager welcome page{" "}
-          <span role="img" aria-label="Greeting icon">
-            💁‍♀️
-          </span>
-        </h1>
+        <h1 style={styles.title}>Welcome to the Phonebook App!</h1>
       </div>
     </>
   );
